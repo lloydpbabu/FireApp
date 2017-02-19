d955215c737e01d030b67dbf6720043c58bacbc5
lloydpbabu <lloydpbabu@gmail.com>
2017-02-19 16:51:38 +0530

    Retrieving list of usernames and showing it as Listview

--- app/src/main/java/com/example/lloyd/fireapp/MainActivity.java
+++ app/src/main/java/com/example/lloyd/fireapp/MainActivity.java
@@ -4,38 +4,57 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mvalueView;
+
     private Firebase mRef;
+    private ArrayList<String> mUsernames = new ArrayList<>();
+    private ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mvalueView= (TextView) findViewById(R.id.valueView);
-        mRef= new Firebase("https://fireapp-d168a.firebaseio.com/");
-        mRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+        mRef= new Firebase("https://fireapp-d168a.firebaseio.com/users");
 
-                Map<String,String> map= dataSnapshot.getValue(Map.class);
-                String name= map.get("Name");
-                String age= map.get("Age");
-                String profession= map.get("Profession");
-                Log.v("E_NAME","Name: "+name);
-                Log.v("E_NAME","Age: "+age);
-                Log.v("E_NAME","Profession: "+profession);
+        mListView= (ListView) findViewById(R.id.listView);
+        final ArrayAdapter<String> arrayAdapter= new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,mUsernames);
+        mListView.setAdapter(arrayAdapter);
+        mRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String value= dataSnapshot.getValue(String.class);
+                mUsernames.add(value);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
 
             }
 
@@ -44,6 +63,9 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+
 
     }
 
