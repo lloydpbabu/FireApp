ffc35d985f2bfe5b50df02dc2ae72ba80546f4d3
lloydpbabu <lloydpbabu@gmail.com>
2017-02-19 16:18:01 +0530

    Logging multiple objects to Android Monitor

--- app/src/main/java/com/example/lloyd/fireapp/MainActivity.java
+++ app/src/main/java/com/example/lloyd/fireapp/MainActivity.java
@@ -2,39 +2,49 @@ package com.example.lloyd.fireapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mvalueField;
-    private Button maddBtn;
-    private EditText mkeyValue;
-    private Firebase mRootRef;
+    private TextView mvalueView;
+    private Firebase mRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mRootRef= new Firebase("https://fireapp-d168a.firebaseio.com/Users");
-        mvalueField= (EditText) findViewById(R.id.valueField);
-        mkeyValue= (EditText) findViewById(R.id.keyValue);
-        maddBtn= (Button) findViewById(R.id.addbtn);
-
-        maddBtn.setOnClickListener(new View.OnClickListener(){
+        mvalueView= (TextView) findViewById(R.id.valueView);
+        mRef= new Firebase("https://fireapp-d168a.firebaseio.com/");
+        mRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view)
-            {
-                String value= mvalueField.getText().toString();
-                String key=mkeyValue.getText().toString();
-                Firebase childRef = mRootRef.child(key);
-                //childRef.setValue(value);
-                //childRef.push().setValue(value);
-                childRef.setValue(value);
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                Map<String,String> map= dataSnapshot.getValue(Map.class);
+                String name= map.get("Name");
+                String age= map.get("Age");
+                String profession= map.get("Profession");
+                Log.v("E_NAME","Name: "+name);
+                Log.v("E_NAME","Age: "+age);
+                Log.v("E_NAME","Profession: "+profession);
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
             }
         });
+
     }
 
 }
